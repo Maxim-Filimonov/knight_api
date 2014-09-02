67e83c92622cdf856d93ee64fe518375f8995b1d
Maxim-Filimonov <tpaktopsp@gmail.com>
2014-09-02 14:48:47 +1000

    add tests for multiple routes

--- spec/knight_api/knight_spec.rb
+++ spec/knight_api/knight_spec.rb
@@ -13,9 +13,17 @@ describe KnightApi::Knight do
     it 'finds single route within two steps' do
       subject = described_class.new(start: 'a1')
 
-      routes = subject.routes_to('e1', limit: 2)
+      routes = subject.routes_to('e1')
 
       expect(routes).to contain_exactly(['a1', 'c2', 'e1'])
+    end
+
+    it 'finds multiple routes within two steps' do
+      subject = described_class.new(start: 'a1')
+
+      routes = subject.routes_to('d4')
+
+      expect(routes).to contain_exactly(['a1', 'b3', 'd4'], ['a1', 'c2', 'd4'])
     end
   end
 end
