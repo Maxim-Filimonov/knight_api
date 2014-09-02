4072f7d2fd7df92c6cadb0167d14b62ffbad2ac0
Maxim-Filimonov <tpaktopsp@gmail.com>
2014-09-02 20:47:39 +1000

    Plug night into new core classes

--- lib/knight_api/knight.rb
+++ lib/knight_api/knight.rb
@@ -1,6 +1,8 @@
 require 'knight_api/point'
 require 'knight_api/coordinate'
 require 'knight_api/path'
+require 'knight_api/path_finder'
+require 'knight_api/knight_mover'
 require 'knight_api/diagnostic'
 
 module KnightApi
@@ -17,62 +19,24 @@ module KnightApi
       destination_position = translate_coordinate(destination)
       debug("Looking for path to #{destination_position} from #{start_position}")
 
-      possible_moves = possible_moves_from(start_position)
-
+      knight_mover = KnightMover.new
+      # possible_moves = knight_mover.possible_moves_from(start_position)
+      path_finder = PathFinder.new(initial_move: start_position,
+                                   destination: destination_position,
+                                   max_moves: opts.fetch(:limit, 6),
+                                   mover: knight_mover,
+                                   debug: @debug)
+      valid_paths = path_finder.find_valid_paths
       found_moves = []
-      possible_moves.each {|move|
-        valid_patches = []
-        path = Path.new
-        result = handle_tree(valid_patches: valid_patches, path: path, move: move, destination_position: destination_position, limit: opts[:limit])
-        valid_patches.each do |path|
-          debug("Valid path - #{path}, #{path}")
-          found_moves << path.moves
-        end
-      }
-      found_moves.inject([]) { |sum, moves|
-        sum << [translate_coordinate_back(start_position), moves.map {|m| translate_coordinate_back(m)}].flatten
+      valid_paths.each do |path|
+        found_moves << path.moves
+      end
+      result = found_moves.inject([]) { |sum, moves|
+        sum << [moves.map {|m| translate_coordinate_back(m)}].flatten
         sum
       }
-    end
+      result
 
-    private
-    def handle_tree(opts={})
-      limit = opts[:limit] || 6
-      path = opts.fetch(:path)
-      move = opts.fetch(:move)
-      destination_position = opts.fetch(:destination_position)
-      valid_patches = opts.fetch(:valid_patches)
-      return nil if path.moves.length >= limit
-      path.add(move)
-      debug("Checking path #{path} for #{move}, level: #{path.moves.length}, limit: #{limit}")
-      if(move.eql?(destination_position))
-        path.valid = true
-        debug("Found valid path #{path}")
-        valid_patches << path
-      else
-        possible_moves = possible_moves_from(move)
-        possible_moves.map { |possible_move|
-          child_path = Path.new(path)
-          handle_tree(valid_patches: valid_patches, path: child_path, move: possible_move, destination_position: destination_position,  limit: limit)
-        }
-      end
-    end
-
-
-    def possible_moves_from(position)
-      possible_moves = []
-      [{x: 1, y: 2}, {x: 2, y: 1}, {x: 2, y: -1}, { x: -1, y: 2}, { x: 1, y: -2}].each do |direction|
-        new_position = move_to({position: position}.merge(direction))
-        possible_moves << new_position if new_position
-      end
-      possible_moves
-    end
-
-    def move_to(opts={})
-      position = opts.fetch(:position)
-      x = opts.fetch(:x)
-      y = opts.fetch(:y)
-      point(x: position.x + x, y: position.y + y)
     end
 
     def point(coords)
@@ -100,5 +64,3 @@ module KnightApi
     end
   end
 end
-
-
--- spec/knight_api/knight_spec.rb
+++ spec/knight_api/knight_spec.rb
@@ -3,7 +3,7 @@ require 'knight_api/knight'
 describe KnightApi::Knight do
   describe '#routes_to' do
     it 'finds single route within one step' do
-      subject = described_class.new(start: 'a1')
+      subject = described_class.new(start: 'a1', debug: true)
 
       routes = subject.routes_to('b3', limit: 1)
 
@@ -36,6 +36,25 @@ describe KnightApi::Knight do
       %w(a1 c2 e1 d3),
       %w(a1 b3 c1 d3),
       %w(a1 c2 b4 d3))
+    end
+
+    it 'finds the example routes' do
+      subject = described_class.new(start: 'a1', debug: true)
+
+      routes = subject.routes_to('d4', limit: 6)
+
+      expect(routes).to contain_exactly(
+                          %w(a1 b3 d4),
+                          %w(a1 c2 d4),
+                          %w(a1 c2 e3 f5 d4),
+                          %w(a1 c2 a3 b5 d4),
+                          %w(a1 c2 b4 c6 d4),
+                          %w(a1 c2 e1 f3 d4),
+                          %w(a1 b3 d2 f3 d4),
+                          %w(a1 b3 c1 e2 d4),
+                          %w(a1 b3 a5 c6 d4),
+                          %w(a1 b3 c5 e6 d4)
+                        )
     end
   end
 end
